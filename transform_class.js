68bd3b6457f8f423b3dad3d85eccfda90ffbbd51
nmal <nmal@naobit.ru>
2016-10-21 13:03:36 +0700

    fix

--- transform_class.js
+++ transform_class.js
@@ -5,12 +5,25 @@ const TransformClass = class TransformClass extends stream.Transform {
 	constructor() {
 		super();
 		this.buffer = [];
+		this.pushTime = new Date().getTime();
 	}
 
 	_transform (chunk, enc, cb) {
+	        /* output only one item per second
 		setTimeout(() => { 
 			this.push(String(Number(chunk) * 2)); cb(); }, 
 		1000);
+		*/
+
+		this.buffer.push(String(Number(chunk) *2));
+
+		let currTime = new Date().getTime();
+		if(currTime - this.pushTime >= 1000) {
+			this.push(this.buffer.join());
+			this.buffer = [];
+			this.pushTime = currTime;
+		}
+		cb();
 	};
 
 }
